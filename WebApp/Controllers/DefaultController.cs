7dc863ce29d69c9258f6f7d663aba182aa3cdc9a
Elias Kokkinos <com.ek@frederick.ac.cy>
2021-01-28 15:24:41 +0200

    Lab 2

---
+++ WebApp/Controllers/DefaultController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Controllers
+{
+    //[Route("api/[controller]")]
+
+    [Route("api/[action]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        public IActionResult GetCourseName()
+        {
+            return Ok("ASCS404");
+        }
+    }
+}
